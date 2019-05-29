87a6378bda22d4a467757aa1caec1125d3798fc4
garjilius <emanuelegar@gmail.com>
2019-05-29 22:59:03 +0200

    Implementata parzialmente una query

--- getQuery.js
+++ getQuery.js
@@ -2,20 +2,45 @@ var driver = neo4j.v1.driver('bolt://localhost', neo4j.v1.auth.basic('neo4j', 'p
 
 var session = driver.session();
 
-let nomeBand = 'The Rolling Stones';
-const result = session.run('MATCH (artist1:Artist {name: $name})-[r:SIMILAR_TO]->(artist2:Artist) return artist2.name', {'name': nomeBand });
-//const collectedNames = [];
+function rightQuery(){
+    console.log("getRightQuery");
+    let chiaveRicerca = document.getElementById("barraRicerca").value;
+    if(document.getElementById("checkArtista").checked) {
+        if(document.getElementById("checkGenere").checked) {
+            console.log("genere");
 
-result.subscribe({
-    onNext: record => {
-        const name = record.get(0);
-        //collectedNames.push(name);
-        addRowToTable(name);
-    },
-    onCompleted: () => {
-        session.close();
-    },
-    onError: error => {
-        console.log(error);
+        }
+        if(document.getElementById("checkGruppo").checked) {
+            matchToSimilarBand(chiaveRicerca);
+            console.log("gruppo "+chiaveRicerca);
+
+        }
     }
-});
+}
+
+function matchToSimilarBand(nomeBand) {
+    flushTable();
+//const collectedNames = [];
+    let query = 'MATCH (artist1:Artist {name:\''+nomeBand+'\'})-[r:SIMILAR_TO]->(artist2:Artist) return artist2.name LIMIT 20';
+    runQuery(query);
+}
+
+function runQuery(query) {
+    addRowToQueryTable(query);
+    const result = session.run(query);
+    result.subscribe({
+        onNext: record => {
+            const name = record.get(0);
+            //collectedNames.push(name);
+            addRowToTable(name);
+        },
+        onCompleted: () => {
+            session.close();
+        },
+        onError: error => {
+            console.log(error);
+        }
+    });
+}
+
+
--- manageTable.js
+++ manageTable.js
@@ -9,3 +9,10 @@ function flushTable() {
     var table = document.getElementById("tabSuggerimentiBody").innerHTML ="";
 
 }
+
+function addRowToQueryTable(query) {
+    var table = document.getElementById("tableQueryBody");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = query;
+}
